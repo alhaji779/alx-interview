d6909e31702a7b77f9def619a8307b4571235bb1
Michael Kanu <michaelokanu01@yahoo.com>
2024-07-25 10:43:09 -0700

    Pascal Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Module to build pascal triangle"""
+
+def pascal_triangle(n):
+    """ Function to create a list of list of a 
+    pascal triangle given an integer n
+    """
+    pascal = []
+    if n <= 0:
+        return []
+    for i in range(n):
+        tr = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                tr.append(1)
+            elif i > 0 and j > 0:
+                tr.append(pascal[i - 1][j - 1] + pascal[i - 1][j])
+        pascal.append(tr)
+    return pascal
